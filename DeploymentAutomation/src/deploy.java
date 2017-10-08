1668428a1fa2bcef45f62fee4df65bbed2b68749
BB <BB@DESKTOP-NP7QMKE>
2017-10-08 11:01:23 -0400

    added method

--- DeploymentAutomation/src/deploy.java
+++ DeploymentAutomation/src/deploy.java
@@ -5,6 +5,9 @@ public class deploy {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void processCSV(String file) {
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		String [][] commands = {
